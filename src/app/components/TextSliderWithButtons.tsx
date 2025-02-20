05c6978f84debfb9a5075f408eec383203589c47
Aegox <diegoamundaray2017@gmail.com>
2025-02-20 10:33:02 -0500

    adjust text in home component an fix buttons in responsive home

--- src/app/components/TextSliderWithButtons.tsx
+++ src/app/components/TextSliderWithButtons.tsx
@@ -19,7 +19,7 @@ const TextSliderWithButtons = () => {
   }, []);
 
   return (
-    <div className="md:pt-[80px] pt-[80px] absolute inset-0 flex flex-col justify-center items-center z-40">
+    <div className="md:pt-[80px] pt-[60px] absolute inset-0 flex flex-col justify-center items-center z-40">
       {/* Slider de texto */}
       <div className="relative w-full max-w-6xl md:h-[25vh] h-[40vh] flex items-center justify-center ">
         {messages.map((message, index) => (
@@ -27,14 +27,14 @@ const TextSliderWithButtons = () => {
             key={index}
             className={`md:px-0 px-[30px] break-words absolute w-full text-center text-white font-light transition-opacity duration-1000 leading-none 
               ${index === currentIndex ? "opacity-100" : "opacity-0"}
-              md:text-[90px] text-6xl`}
+              md:text-[80px] text-5xl`}
           >
             {message}
           </p>
         ))}
       </div>
       {/* Botones */}
-      <div className="gap-1 md:mt-10 mt-14 flex flex-wrap justify-center items-center space-x-4 overflow-x-auto ">
+      <div className="md:gap-4 gap-2 md:mt-12 mt-14 flex flex-wrap justify-center items-center w-70%">
         <button className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-4 px-8 rounded-lg shadow-lg transition-colors text-xl whitespace-nowrap">
           Prueba Gratis
         </button>
