2b09e7091ee3499eb83fa19be5a041ceea3a798a
Aegox <diegoamundaray2017@gmail.com>
2025-03-13 15:22:14 -0500

    fix import in questions component

--- src/app/components/Questions.tsx
+++ src/app/components/Questions.tsx
@@ -5,7 +5,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import faqData from "../constants/Questions.ts";
+import { faqData } from "../constants/Questions.ts";
 
 const Questions: React.FC = () => {
   
